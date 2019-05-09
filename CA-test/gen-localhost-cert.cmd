351cb69c693b798742d610cdb54af204d9b16e44
Chris Iverson <cj.no.one@gmail.com>
2019-05-09 11:35:59 -0500

    Updating test CA generation script to handle duplicate certs, as well as generate a test PFX file with a password.

--- CA-test/gen-localhost-cert.cmd
+++ CA-test/gen-localhost-cert.cmd
@@ -10,4 +10,6 @@ openssl req -new -config localhost.conf -key localhost\localhost.key -out localh
 
 openssl ca -batch -config sub-ca.conf -in localhost\localhost.csr -out localhost\localhost.crt -extensions server_cert
 
-openssl pkcs12 -export -in localhost\localhost.crt -out localhost\localhost.pfx -inkey localhost\localhost.key -passout pass:
+openssl pkcs12 -export -in localhost\localhost.crt -out localhost\localhost.pfx -inkey localhost\localhost.key -certfile sub-ca\certs\sub-ca.crt -passout pass:
+
+openssl pkcs12 -export -in localhost\localhost.crt -out localhost\localhost-test.pfx -inkey localhost\localhost.key -certfile sub-ca\certs\sub-ca.crt -passout pass:test
